5d0d75219154e973b5b3b8d57a86f6cc471190e8
Ether <ether.factor@gmail.com>
2023-06-21 21:32:27 -0500

    Added a database migration project

---
+++ EtherGizmos.SqlMonitor.Database/Core/OneWayMigration.cs
@@ -0,0 +1,15 @@
+﻿using FluentMigrator;
+
+namespace EtherGizmos.SqlMonitor.Database.Core;
+
+/// <summary>
+/// Allows migration forward, but not backward.
+/// </summary>
+public abstract class OneWayMigration : Migration
+{
+    /// <inheritdoc/>
+    public override void Down()
+    {
+        throw new InvalidOperationException("This migration cannot be reversed. To downgrade, a SQL restore must be performed.");
+    }
+}
