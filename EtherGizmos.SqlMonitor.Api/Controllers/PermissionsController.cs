6a2cdf2e3767d8e9b3b04c863573df671314acca
Ether <ether.factor@gmail.com>
2023-07-03 23:24:43 -0500

    Migration tweaks

--- EtherGizmos.SqlMonitor.Api/Controllers/PermissionsController.cs
+++ EtherGizmos.SqlMonitor.Api/Controllers/PermissionsController.cs
@@ -10,23 +10,45 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EtherGizmos.SqlMonitor.Api.Controllers;
 
+/// <summary>
+/// Provides endpoints for <see cref="Permission"/> records.
+/// </summary>
 [Route(BasePath)]
 public class PermissionsController : ODataController
 {
     private const string BasePath = "/api/v1/permissions";
 
+    /// <summary>
+    /// Allows conversion between database and DTO models.
+    /// </summary>
     private IMapper Mapper { get; }
 
+    /// <summary>
+    /// Provides access to the storage of records.
+    /// </summary>
     private IPermissionService PermissionService { get; }
 
+    /// <summary>
+    /// Queries stored records.
+    /// </summary>
     private IQueryable<Permission> Permissions => PermissionService.GetQueryable();
 
+    /// <summary>
+    /// Constructs the controller.
+    /// </summary>
+    /// <param name="mapper">Allows conversion between database and DTO models.</param>
+    /// <param name="permissionService">Provides access to the storage of records.</param>
     public PermissionsController(IMapper mapper, IPermissionService permissionService)
     {
         Mapper = mapper;
         PermissionService = permissionService;
     }
 
+    /// <summary>
+    /// Searches all records.
+    /// </summary>
+    /// <param name="queryOptions">The query options to use.</param>
+    /// <returns>An awaitable task.</returns>
     [HttpGet]
     [Route(BasePath)]
     public async Task<IActionResult> Search(ODataQueryOptions<PermissionDTO> queryOptions)
@@ -35,6 +57,12 @@ public class PermissionsController : ODataController
         return Ok(finished);
     }
 
+    /// <summary>
+    /// Finds a single record.
+    /// </summary>
+    /// <param name="id">The id of the record.</param>
+    /// <param name="queryOptions">The query options to use.</param>
+    /// <returns>An awaitable task.</returns>
     [HttpGet]
     [Route(BasePath + "({id})")]
     public async Task<IActionResult> Get(string id, ODataQueryOptions<PermissionDTO> queryOptions)
