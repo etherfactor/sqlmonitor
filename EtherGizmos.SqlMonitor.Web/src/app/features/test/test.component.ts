3ed6e9364db1ba6898921b0be99930f7e722e662
Ether <ether.factor@gmail.com>
2023-11-16 22:35:20 -0600

    Configure basic chart

--- EtherGizmos.SqlMonitor.Web/src/app/features/test/test.component.ts
+++ EtherGizmos.SqlMonitor.Web/src/app/features/test/test.component.ts
@@ -1,13 +1,130 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BaseChartDirective, NgChartsModule } from 'ng2-charts';
+import { ChartConfiguration, ChartType } from 'chart.js';
+import { ChartEvent } from 'chart.js/dist/plugins/plugin.tooltip';
 
 @Component({
   selector: 'app-test',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    NgChartsModule,
+  ],
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
 export class TestComponent {
 
+  public lineChartData: ChartConfiguration['data'] = {
+    datasets: [
+      {
+        data: [65, 59, 80, 81, 56, 55, 40],
+        label: 'Series A',
+        backgroundColor: 'rgba(148,159,177,0.2)',
+        borderColor: 'rgba(148,159,177,1)',
+        pointBackgroundColor: 'rgba(148,159,177,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+        fill: 'origin',
+      },
+      {
+        data: [28, 48, 40, 19, 86, 27, 90],
+        label: 'Series B',
+        backgroundColor: 'rgba(77,83,96,0.2)',
+        borderColor: 'rgba(77,83,96,1)',
+        pointBackgroundColor: 'rgba(77,83,96,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(77,83,96,1)',
+        fill: 'origin',
+      },
+      {
+        data: [180, 480, 770, 90, 1000, 270, 400],
+        label: 'Series C',
+        yAxisID: 'y1',
+        backgroundColor: 'rgba(255,0,0,0.3)',
+        borderColor: 'red',
+        pointBackgroundColor: 'rgba(148,159,177,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+        fill: 'origin',
+      },
+    ],
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  };
+
+  public lineChartOptions: ChartConfiguration['options'] = {
+    elements: {
+      line: {
+        tension: 0.5,
+      },
+    },
+    scales: {
+      // We use this empty structure as a placeholder for dynamic theming.
+      y: {
+        position: 'left',
+      },
+      y1: {
+        position: 'right',
+        grid: {
+          color: 'rgba(255,0,0,0.3)',
+        },
+        ticks: {
+          color: 'red',
+        },
+      },
+    },
+
+    plugins: {
+      legend: { display: true },
+      //annotation: {
+      //  annotations: [
+      //    {
+      //      type: 'line',
+      //      scaleID: 'x',
+      //      value: 'March',
+      //      borderColor: 'orange',
+      //      borderWidth: 2,
+      //      label: {
+      //        display: true,
+      //        position: 'center',
+      //        color: 'orange',
+      //        content: 'LineAnno',
+      //        font: {
+      //          weight: 'bold',
+      //        },
+      //      },
+      //    },
+      //  ],
+      //},
+    },
+  };
+
+  public lineChartType: ChartType = 'line';
+
+  @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
+
+  // events
+  public chartClicked({
+    event,
+    active,
+  }: {
+    event?: ChartEvent;
+    active?: object[];
+  }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({
+    event,
+    active,
+  }: {
+    event?: ChartEvent;
+    active?: object[];
+  }): void {
+    console.log(event, active);
+  }
 }
