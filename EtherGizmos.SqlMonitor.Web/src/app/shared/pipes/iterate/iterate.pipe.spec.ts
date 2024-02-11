2e5e1bbf58f31af1611b492aa4336c16907d78a9
Ether <ether.factor@gmail.com>
2024-02-11 15:30:18 -0600

    Add tests to iterate

--- EtherGizmos.SqlMonitor.Web/src/app/shared/pipes/iterate/iterate.pipe.spec.ts
+++ EtherGizmos.SqlMonitor.Web/src/app/shared/pipes/iterate/iterate.pipe.spec.ts
@@ -1,8 +1,25 @@
+import { TestBed } from '@angular/core/testing';
 import { IteratePipe } from './iterate.pipe';
 
 describe('IteratePipe', () => {
-  it('create an instance', () => {
-    const pipe = new IteratePipe();
+  let pipe: IteratePipe;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [IteratePipe]
+    })
+    .compileComponents();
+
+    pipe = new IteratePipe();
+  });
+
+  it('should create an instance', () => {
     expect(pipe).toBeTruthy();
+  });
+
+  it('should transform a number into an array', () => {
+    const result = pipe.transform(10);
+    expect(result.length).toBe(10);
+    expect(result).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
   });
 });
