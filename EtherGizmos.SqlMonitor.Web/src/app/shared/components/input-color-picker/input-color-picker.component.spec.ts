e3d8b9bfaf8902e9a6dfee3a38853879342b6f84
Ether <ether.factor@gmail.com>
2024-02-11 14:46:19 -0600

    Add tests to input-color-picker

--- EtherGizmos.SqlMonitor.Web/src/app/shared/components/input-color-picker/input-color-picker.component.spec.ts
+++ EtherGizmos.SqlMonitor.Web/src/app/shared/components/input-color-picker/input-color-picker.component.spec.ts
@@ -1,5 +1,6 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { InputColorPickerComponent } from './input-color-picker.component';
 
 describe('InputColorPickerComponent', () => {
@@ -19,5 +20,43 @@ describe('InputColorPickerComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should open dropdown when color picker is clicked', () => {
+    let element: DebugElement;
+    let styles: CSSStyleDeclaration;
+    const button = fixture.debugElement.query(By.css('[data-testid="color-picker-button"]'));
+
+    element = fixture.debugElement.query(By.css('[data-testid="color-picker"]'));
+    styles = window.getComputedStyle(element.nativeElement);
+    expect(styles['display']).toBe('none');
+
+    button.nativeElement.click();
+    fixture.detectChanges();
+
+    element = fixture.debugElement.query(By.css('[data-testid="color-picker"]'));
+    styles = window.getComputedStyle(element.nativeElement);
+    expect(styles['display']).not.toBe('none');
+  });
+
+  it('should change color when value changes', () => {
+    let buttonBackround: string;
+
+    buttonBackround = component.dynamicPickerStyle['--bs-btn-bg'];
+    expect(buttonBackround).toBe('#ffffff');
+
+    component.writeValue('#ff0000');
+    fixture.detectChanges();
+
+    buttonBackround = component.dynamicPickerStyle['--bs-btn-bg'];
+    expect(buttonBackround).toBe('#ff0000');
+  });
+
+  it('should bind to ngModel', () => {
+    component.registerOnChange(value => {
+      expect(value).toBe('#ff0000');
+    });
+
+    component.writeValue('#ff0000');
   });
 });
