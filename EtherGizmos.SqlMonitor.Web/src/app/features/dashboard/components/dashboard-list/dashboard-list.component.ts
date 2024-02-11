7092abdd827af303bc4bd93343219ced59d7aab9
Ether <ether.factor@gmail.com>
2024-02-10 23:54:36 -0600

    Add folder and dashboard cards

--- EtherGizmos.SqlMonitor.Web/src/app/features/dashboard/components/dashboard-list/dashboard-list.component.ts
+++ EtherGizmos.SqlMonitor.Web/src/app/features/dashboard/components/dashboard-list/dashboard-list.component.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { NavbarMenuService } from '../../../../shared/services/navbar-menu/navbar-menu.service';
+import { IteratePipe } from '../../../../shared/pipes/iterate/iterate.pipe';
 import { BodyContainerType, BodyService } from '../../../../shared/services/body/body.service';
+import { NavbarMenuService } from '../../../../shared/services/navbar-menu/navbar-menu.service';
 
 @Component({
   selector: 'app-dashboard-list',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    IteratePipe,
+  ],
   templateUrl: './dashboard-list.component.html',
   styleUrl: './dashboard-list.component.scss'
 })
@@ -23,7 +28,7 @@ export class DashboardListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.$body.setContainer(BodyContainerType.Fluid);
+    this.$body.setContainer(BodyContainerType.Normal);
     this.updateBreadcrumbs();
     this.updateActions();
   }
