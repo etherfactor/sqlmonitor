46c1540badde799b856c045b26ea533c8d69a318
Ether <ether.factor@gmail.com>
2024-02-12 19:56:44 -0600

    Stabilize app tests

--- EtherGizmos.SqlMonitor.Web/src/app/app.component.spec.ts
+++ EtherGizmos.SqlMonitor.Web/src/app/app.component.spec.ts
@@ -1,31 +1,34 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { APP_ROUTES } from './app.routes';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      imports: [
+        AppComponent,
+        RouterTestingModule.withRoutes(APP_ROUTES),
+      ]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+
+    const router = TestBed.inject(Router);
+    await router.navigate(['/']);
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'EtherGizmos.SqlMonitor.Web'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app.title).toEqual('EtherGizmos.SqlMonitor.Web');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('EtherGizmos.SqlMonitor.Web app is running!');
   });
 });
