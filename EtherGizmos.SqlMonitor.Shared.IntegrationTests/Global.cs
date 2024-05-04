70f598f25fcba2fd252affff829e419144179d03
Ether <ether.factor@gmail.com>
2024-05-04 17:53:23 -0500

    Fix broken test project

--- EtherGizmos.SqlMonitor.Shared.IntegrationTests/Global.cs
+++ EtherGizmos.SqlMonitor.Shared.IntegrationTests/Global.cs
@@ -1,14 +1,3 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 
 [assembly: ExcludeFromCodeCoverage]
-
-namespace EtherGizmos.SqlMonitor.Shared.IntegrationTests;
-
-internal class GenericTest()
-{
-    [Test]
-    public void Test()
-    {
-
-    }
-}
