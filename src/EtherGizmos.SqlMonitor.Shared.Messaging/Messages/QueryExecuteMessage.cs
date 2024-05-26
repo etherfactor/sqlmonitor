9fa8ef809aed837dbd0f7470542719fdda1c3c30
Ether <ether.factor@gmail.com>
2024-05-26 11:30:53 -0500

    Add messages for executing queries and returning results

--- src/EtherGizmos.SqlMonitor.Shared.Messaging/Messages/QueryExecuteMessage.cs
+++ src/EtherGizmos.SqlMonitor.Shared.Messaging/Messages/QueryExecuteMessage.cs
@@ -1,14 +1,26 @@
-﻿using EtherGizmos.SqlMonitor.Shared.Models.Database;
+﻿using EtherGizmos.SqlMonitor.Shared.Models.Database.Enums;
 
 namespace EtherGizmos.SqlMonitor.Shared.Messaging.Messages;
 
 public class QueryExecuteMessage
 {
+    public Guid QueryId { get; set; }
+
+    public string Name { get; set; }
+
     public int MonitoredQueryTargetId { get; set; }
 
     public string ConnectionRequestToken { get; set; }
 
-    public QueryVariant QueryVariant { get; set; }
+    public SqlType SqlType { get; set; }
+
+    public string QueryText { get; set; }
+
+    public string? BucketColumn { get; set; }
+
+    public string? TimestampUtcColumn { get; set; }
+
+    public List<Metric> Metrics { get; set; } = new();
 
     /// <summary>
     /// Not intended for direct use.
@@ -16,16 +28,57 @@ public class QueryExecuteMessage
     public QueryExecuteMessage()
     {
         ConnectionRequestToken = null!;
-        QueryVariant = null!;
+        Name = null!;
+        QueryText = null!;
     }
 
     public QueryExecuteMessage(
+        Guid queryId,
+        string name,
         int monitoredQueryTargetId,
         string connectionRequestToken,
-        QueryVariant queryVariant)
+        SqlType sqlType,
+        string queryText,
+        string? bucketColumn,
+        string? timestampUtcColumn)
     {
+        QueryId = queryId;
+        Name = name;
         MonitoredQueryTargetId = monitoredQueryTargetId;
         ConnectionRequestToken = connectionRequestToken;
-        QueryVariant = queryVariant;
+        SqlType = sqlType;
+        QueryText = queryText;
+        BucketColumn = bucketColumn;
+        TimestampUtcColumn = timestampUtcColumn;
+    }
+
+    public void AddMetric(
+        int metricId,
+        string valueColumn)
+    {
+        Metrics.Add(new(metricId, valueColumn));
+    }
+
+    public class Metric
+    {
+        public int MetricId { get; set; }
+
+        public string ValueColumn { get; set; }
+
+        /// <summary>
+        /// Not intended for direct use.
+        /// </summary>
+        public Metric()
+        {
+            ValueColumn = null!;
+        }
+
+        public Metric(
+            int metricId,
+            string valueColumn)
+        {
+            MetricId = metricId;
+            ValueColumn = valueColumn;
+        }
     }
 }
---
+++ src/EtherGizmos.SqlMonitor.Shared.Messaging/Messages/QueryResultMessage.cs
@@ -0,0 +1,66 @@
+﻿namespace EtherGizmos.SqlMonitor.Shared.Messaging.Messages;
+
+public class QueryResultMessage
+{
+    public Guid QueryId { get; set; }
+
+    public string Name { get; set; }
+
+    public int MonitoredQueryTargetId { get; set; }
+
+    public List<MetricValue> MetricValues { get; set; } = new();
+
+    public QueryResultMessage()
+    {
+        Name = null!;
+    }
+
+    public QueryResultMessage(
+        Guid queryId,
+        string name,
+        int monitoredQueryTargetId)
+    {
+        QueryId = queryId;
+        Name = name;
+        MonitoredQueryTargetId = monitoredQueryTargetId;
+    }
+
+    public void AddMetricValue(
+        int metricId,
+        string? bucket,
+        DateTimeOffset? timestampUtc,
+        double value)
+    {
+        MetricValues.Add(new(metricId, bucket, timestampUtc, value));
+    }
+
+    public class MetricValue
+    {
+        public int MetricId { get; set; }
+
+        public string? Bucket { get; set; }
+
+        public DateTimeOffset? TimestampUtc { get; set; }
+
+        public double Value { get; set; }
+
+        /// <summary>
+        /// Not intended for direct use.
+        /// </summary>
+        public MetricValue()
+        {
+        }
+
+        public MetricValue(
+            int metricId,
+            string? bucket,
+            DateTimeOffset? timestampUtc,
+            double value)
+        {
+            MetricId = metricId;
+            Bucket = bucket;
+            TimestampUtc = timestampUtc;
+            Value = value;
+        }
+    }
+}
