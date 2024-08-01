daa1d5a97cbeea041f5cf448de338f44dd2f01b2
Ether <ether.factor@gmail.com>
2024-07-31 22:28:40 -0500

    Clean up filtering and produce filter object

--- src/EtherGizmos.SqlMonitor.Web/src/app/shared/components/table-header/table-header.component.ts
+++ src/EtherGizmos.SqlMonitor.Web/src/app/shared/components/table-header/table-header.component.ts
@@ -1,11 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnDestroy, OnInit, Optional } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit, Optional, SimpleChanges } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgSelectModule } from '@ng-select/ng-select';
 import { NgxMaskDirective } from 'ngx-mask';
+import { Subscription } from 'rxjs';
 import { SortTableService } from '../../services/sort-table/sort-table.service';
 import { generateGuid } from '../../types/guid/guid';
+import { FilterCondition, FilterPropertyOperator, FilterType, defaultOperators, displayText, filterConditionForm, showInput } from '../../utilities/filter/filter.util';
+import { TypedFormGroup } from '../../utilities/form/form.util';
 import { Direction } from '../../utilities/odata/odata.util';
-import { FilterType } from '../filter-builder-modal/filter-builder-modal.component';
 import { InputLuxonDatetimeComponent } from '../input-luxon-datetime/input-luxon-datetime.component';
 import { TableComponent } from '../table/table.component';
 
@@ -14,9 +18,12 @@ import { TableComponent } from '../table/table.component';
   standalone: true,
   imports: [
     CommonModule,
+    FormsModule,
     InputLuxonDatetimeComponent,
     NgbDropdownModule,
+    NgSelectModule,
     NgxMaskDirective,
+    ReactiveFormsModule,
   ],
   templateUrl: './table-header.component.html',
   styleUrl: './table-header.component.scss',
@@ -26,7 +33,7 @@ import { TableComponent } from '../table/table.component';
     //'(mouseleave)': 'onMouseLeave($event)',
   }
 })
-export class TableHeaderComponent<TData extends object> implements OnInit, OnDestroy {
+export class TableHeaderComponent<TData extends object> implements OnInit, OnChanges, OnDestroy {
 
   @Input({ alias: 'app-table-header', required: true }) name!: string;
 
@@ -36,10 +43,14 @@ export class TableHeaderComponent<TData extends object> implements OnInit, OnDes
 
   @Input() filterable: boolean = false;
 
+  private readonly $form: FormBuilder;
   private readonly $sortTable: SortTableService;
   private readonly table: TableComponent<TData>;
 
   private id = generateGuid();
+
+  filterForm: TypedFormGroup<FilterCondition>;
+  filterSubscriptions: Subscription[] = [];
 
   get direction() {
     return this.$sortTable.getSortDirection(this.id, this.name);
@@ -60,11 +71,15 @@ export class TableHeaderComponent<TData extends object> implements OnInit, OnDes
   };
 
   constructor(
+    $form: FormBuilder,
     $sortTable: SortTableService,
     @Optional() table: TableComponent<TData>,
   ) {
+    this.$form = $form;
     this.$sortTable = $sortTable;
     this.table = table;
+
+    this.filterForm = filterConditionForm(this.$form, { operator: undefined!, value: undefined });
   }
 
   ngOnInit(): void {
@@ -73,8 +88,30 @@ export class TableHeaderComponent<TData extends object> implements OnInit, OnDes
     }
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    const condition = this.filterForm;
+    const operatorSub = condition.controls.operator.valueChanges.subscribe(operator => {
+      const value = condition.controls.value;
+      value.setValue(undefined);
+
+      if (operator && showInput[operator]) {
+        value.setValidators([Validators.required]);
+      } else {
+        value.setValidators([]);
+      }
+
+      value.updateValueAndValidity();
+    });
+
+    this.filterSubscriptions.push(operatorSub);
+  }
+
   ngOnDestroy(): void {
     this.$sortTable.setSortDirection(this.id, undefined);
+
+    for (const subscription of this.filterSubscriptions) {
+      subscription.unsubscribe();
+    }
   }
 
   stopPropagation($event: Event) {
@@ -99,5 +136,30 @@ export class TableHeaderComponent<TData extends object> implements OnInit, OnDes
 
   onMouseLeave() {
     console.log('leave');
+  }
+
+  getOperators(): FilterPropertyOperator[] {
+    return defaultOperators[this.type ?? 'string'];
+  }
+
+  getOperatorDisplayName(operator: FilterPropertyOperator) {
+    return displayText[this.type ?? 'string'][operator];
+  }
+
+  showOperatorValue() {
+    const condition = this.filterForm;
+    return showInput[condition?.controls?.operator?.value ?? '' as FilterPropertyOperator] ?? false;
+  }
+
+  getFilterCondition() {
+    const condition = this.filterForm;
+    if (condition.invalid)
+      return undefined;
+
+    const value = condition.value;
+    if (!value.operator)
+      return undefined;
+
+    return value;
   }
 }
---
+++ src/EtherGizmos.SqlMonitor.Web/src/app/shared/utilities/filter/filter.util.ts
@@ -0,0 +1,130 @@
+import { FormBuilder, Validators } from "@angular/forms";
+import { DefaultControlTypes, formFactoryForModel } from "../form/form.util";
+
+export type FilterOperator = 'and' | 'or';
+
+export interface FilterCondition {
+  operator: FilterPropertyOperator;
+  value: any;
+}
+
+export type FilterType = 'boolean' | 'datetime' | 'guid' | 'number' | 'string';
+
+export type FilterPropertyOperator = 'equals' | 'not_equals' | 'greater' | 'greater_equals'
+  | 'less' | 'less_equals' | 'starts_with' | 'ends_with' | 'contains' | 'not_contains'
+  | 'true' | 'false' | 'null' | 'not_null';
+
+export const defaultOperators: { [key in FilterType]: FilterPropertyOperator[] } = {
+  boolean: ['true', 'false', 'null', 'not_null'],
+  datetime: ['equals', 'not_equals', 'greater_equals', 'less_equals', 'null', 'not_null'],
+  guid: ['equals', 'not_equals', 'null', 'not_null'],
+  number: ['equals', 'not_equals', 'greater', 'greater_equals', 'less', 'less_equals', 'null', 'not_null'],
+  string: ['equals', 'not_equals', 'starts_with', 'ends_with', 'contains', 'not_contains', 'null', 'not_null'],
+};
+
+export const showInput: { [key in FilterPropertyOperator]: boolean } = {
+  contains: true,
+  ends_with: true,
+  equals: true,
+  false: false,
+  greater: true,
+  greater_equals: true,
+  less: true,
+  less_equals: true,
+  not_contains: true,
+  not_equals: true,
+  not_null: false,
+  null: false,
+  starts_with: true,
+  true: false,
+};
+
+export const displayText: { [key in FilterType]: { [key in FilterPropertyOperator]: string } } = {
+  boolean: {
+    contains: '',
+    ends_with: '',
+    equals: '',
+    false: 'is false',
+    greater: '',
+    greater_equals: '',
+    less: '',
+    less_equals: '',
+    not_contains: '',
+    not_equals: '',
+    not_null: 'is not null',
+    null: 'is null',
+    starts_with: '',
+    true: 'is true',
+  },
+  datetime: {
+    contains: '',
+    ends_with: '',
+    equals: 'at',
+    false: '',
+    greater: '',
+    greater_equals: 'after',
+    less: '',
+    less_equals: 'before',
+    not_contains: '',
+    not_equals: 'not at',
+    not_null: 'is not null',
+    null: 'is null',
+    starts_with: '',
+    true: '',
+  },
+  guid: {
+    contains: '',
+    ends_with: '',
+    equals: 'equals',
+    false: '',
+    greater: '',
+    greater_equals: '',
+    less: '',
+    less_equals: '',
+    not_contains: '',
+    not_equals: 'does not equal',
+    not_null: 'is not null',
+    null: 'is null',
+    starts_with: '',
+    true: '',
+  },
+  number: {
+    contains: '',
+    ends_with: '',
+    equals: 'is equal to',
+    false: '',
+    greater: 'greater than',
+    greater_equals: 'greater than or equal to',
+    less: 'less than',
+    less_equals: 'less than or equal to',
+    not_contains: '',
+    not_equals: 'does not equal',
+    not_null: 'is not null',
+    null: 'is null',
+    starts_with: '',
+    true: '',
+  },
+  string: {
+    contains: 'contains',
+    ends_with: 'ends with',
+    equals: 'is equal to',
+    false: '',
+    greater: '',
+    greater_equals: '',
+    less: '',
+    less_equals: '',
+    not_contains: 'does not contain',
+    not_equals: 'does not equal',
+    not_null: 'is not null',
+    null: 'is null',
+    starts_with: 'starts with',
+    true: '',
+  },
+};
+
+export const filterConditionForm = formFactoryForModel<FilterCondition, DefaultControlTypes>(($form: FormBuilder, model: FilterCondition) => {
+  return {
+    operator: [model.operator],
+    value: [model.value, Validators.required],
+  };
+});
