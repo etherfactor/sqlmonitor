6414efde3ee357f9b49ac7bbed91eb0e46d80809
Ryan Duffy <ether.factor@gmail.com>
2024-08-28 10:12:17 -0500

    Utilize signal store for totals

--- src/EtherGizmos.SqlMonitor.Web/src/app/features/administration/pages/home/home.component.ts
+++ src/EtherGizmos.SqlMonitor.Web/src/app/features/administration/pages/home/home.component.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, computed, signal } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ActivityCardComponent } from '../../../../shared/components/activity-card/activity-card.component';
 import { BodyContainerType, BodyService } from '../../../../shared/services/body/body.service';
-import { MonitoredEnvironmentService } from '../../../../shared/services/monitored-environment/monitored-environment.service';
+import { MonitoredEnvironmentService, MonitoredEnvironmentStore } from '../../../../shared/services/monitored-environment/monitored-environment.service';
 import { MonitoredResourceService } from '../../../../shared/services/monitored-resource/monitored-resource.service';
 import { MonitoredSystemService } from '../../../../shared/services/monitored-system/monitored-system.service';
 import { NavbarMenuAction, NavbarMenuBreadcrumb, NavbarMenuService } from '../../../../shared/services/navbar-menu/navbar-menu.service';
-import { o } from '../../../../shared/utilities/odata/odata.util';
 
 @Component({
   selector: 'app-home',
@@ -24,14 +23,13 @@ export class HomeComponent implements OnInit {
 
   private readonly $body: BodyService;
   private readonly $monitoredEnvironment: MonitoredEnvironmentService;
+  private readonly $monitoredEnvironmentStore = inject(MonitoredEnvironmentStore);
   private readonly $monitoredResource: MonitoredResourceService;
   private readonly $monitoredSystem: MonitoredSystemService;
   private readonly $navbarMenu: NavbarMenuService;
 
-  loadingEnvironments$$ = computed(() => this.activeEnvironments$$() === undefined || this.inactiveEnvironments$$() === undefined);
-  activeEnvironments$$ = signal<number | undefined>(undefined);
-  inactiveEnvironments$$ = signal<number | undefined>(undefined);
-  totalEnvironments$$ = computed(() => (this.activeEnvironments$$() ?? 0) + (this.inactiveEnvironments$$() ?? 0));
+  loadingEnvironments$$ = this.$monitoredEnvironmentStore.isLoading;
+  environmentStatuses$$ = this.$monitoredEnvironmentStore.states;
 
   constructor(
     $body: BodyService,
@@ -52,29 +50,7 @@ export class HomeComponent implements OnInit {
     this.$navbarMenu.setActions(this.actions);
     this.$navbarMenu.setBreadcrumbs(this.breadcrumbs);
 
-    this.$monitoredEnvironment.set
-      .filter(e =>
-        o.eq(
-          e.prop('isActive'),
-          o.bool(true),
-        ),
-      )
-      .top(0)
-      .count()
-      .execute()
-      .subscribe(aEnv => this.activeEnvironments$$.set(aEnv['@odata.count']));
-
-    this.$monitoredEnvironment.set
-      .filter(e =>
-        o.ne(
-          e.prop('isActive'),
-          o.bool(true),
-        )
-      )
-      .top(0)
-      .count()
-      .execute()
-      .subscribe(iEnv => this.inactiveEnvironments$$.set(iEnv['@odata.count']));
+    this.$monitoredEnvironmentStore.loadTotals();
   }
 
   protected get actions(): NavbarMenuAction[] {
